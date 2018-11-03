24befb57bbda44da9fb01bf9174c9cfd28b0daa5
digiz3d <1338620+digiz3d@users.noreply.github.com>
2018-11-03 21:06:10 +0100

    more UI

--- src/Screens/HistoryScreen.jsx
+++ src/Screens/HistoryScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HistoryScreen.css';
+import HistoryIcon from '../Assets/Time.svg';
 import LeftIcon from '../Assets/Left.svg';
 import DeleteIcon from '../Assets/Delete.svg';
 
@@ -22,7 +23,14 @@ class HistoryScreen extends React.Component {
     render() {
         return (
             <div className="HistoryScreen">
-                <h1>History</h1>
+                <div className="ButtonsContainer">
+                    <img
+                        className="Button"
+                        alt="Back"
+                        src={HistoryIcon}
+                        style={{ width: 40, height: 40 }}
+                    />
+                </div>
                 <div className="Entries">
                     <p className="Entry">test</p>
                     <p className="Entry">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -34,7 +42,7 @@ class HistoryScreen extends React.Component {
                     <p className="Entry">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p className="Entry">DDDDDDDDDDDre magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
-                <div className="BottomButtons">
+                <div className="ButtonsContainer">
                     <img
                         className="Button"
                         onClick={this.onClickGoBack}
