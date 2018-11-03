1be66a1169c7760410395612d809a9411f7ba93a
digiz3d <1338620+digiz3d@users.noreply.github.com>
2018-11-03 14:11:16 +0100

    more UI work

---
+++ src/Screens/MainSwitch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Router } from 'react-router-dom';
+
+
+class MainSwitch extends Component {
+    render() {
+        return(
+            <Router>
+                <React.Fragment>
+                    
+                </React.Fragment>
+            </Router>
+        );
+    }
+}
+
+export default MainSwitch;
